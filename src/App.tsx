2cf8b90b88131de4c838e9fe60cf491ec772310a
jeseias <domingosjeseias@gmail.com>
2024-02-04 15:25:36 +0100

    feat: add axios and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import { queryClient } from "./lib/query-client";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

ad3b102fc5b045a270476fdd1b247f0233e58b55
jeseias <domingosjeseias@gmail.com>
2024-02-06 21:07:07 +0100

    fox

--- src/services/use-api-create-client.ts
+++ src/services/use-api-create-client.ts
@@ -31,6 +31,7 @@ export const useApiCreateClient = () => {
         description: "New client added",
       });
     },
+    onError() {
       toast({
         title: "Error",
         description: "Error while creating client",
